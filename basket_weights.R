3a3892698a50245a1df998bde4ab329efd9b7844
RebeccaHaehn-NOAA <65730849+RebeccaHaehn-NOAA@users.noreply.github.com>
2025-11-17 15:55:35 -0800

    first push, script runs, next step clean and annotate

---
+++ basket_weights.R
@@ -0,0 +1,31 @@
+
+
+library(tidyverse)
+library(here) #maynot need
+
+# select METIS_LOAD_CSAMP_HAUL#.txt file
+if (requireNamespace("rstudioapi", quietly = TRUE)) {
+  species_haul <- rstudioapi::selectFile(caption = "Select _CASAMP_HAUL file", 
+                                 existing = TRUE)
+ 
+} 
+
+# import haul species file
+haul_species <- read.delim(species_haul, sep = ",", header = FALSE)
+
+# select METIS_LOAD_CVAL_HAUL#.txt file
+if (requireNamespace("rstudioapi", quietly = TRUE)) {
+  basket_haul <- rstudioapi::selectFile(caption = "Select _CVAL_HAUL file", 
+                                         #filter = "Text Files (*.txt);;All Files (*.*)", 
+                                         existing = TRUE)
+  
+} 
+
+#import basket weights
+basket_weights <- read.delim(basket_haul, sep = ",", header = FALSE)
+
+#join basket weights to species information in haul file
+species_basket_weights <- basket_weights %>%
+  dplyr::left_join(haul_species, by = c("V7" = "V6")) %>%
+  dplyr::select(join_on = V7, species = V12.y, species_code = V11.y, imput_order = V6, weight = V9.x, unit = V10.x, count = V11.x, sample_type = V13.x, timestamp = V14.x)
+
